59c71fa904b965b5943b20639aaef18427d28d09
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-05 20:43:34 +0100

    usertoken.py now returns all within one line

--- usertoken.py
+++ usertoken.py
@@ -1,11 +1,7 @@
+from helpers.time import Time
+from helpers.random import Random
 from helpers.encoding import Encode
 
 class UserToken:
-    def generate(user_id: int) -> str:
-        token_structure: str = f""
-        
-        token_structure += f"{Encode.string(str(user_id))}."
-        print(token_structure)
-        
-if __name__ == '__main__':
-    UserToken.generate(80088516616269824)
+    def generate(user_id: int) -> str:    
+        return f"{Encode.string(str(user_id))}.{Encode.string(str(Time.seconds_since_epoch(2023, 1, 6)))}.{Encode.string(Random.string())}"

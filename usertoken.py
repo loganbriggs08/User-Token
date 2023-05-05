e0f029e39caea069a032c4ee8c530d9c48c075e0
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-05 20:20:23 +0100

    Add test script to test our encoding

--- usertoken.py
+++ usertoken.py
@@ -1,3 +1,9 @@
+from helpers.encoding import Encode
+
 class UserToken:
-    def generate() -> str:
-        print("Here")
+    def generate(user_id: int) -> str:
+        encoded_user_id = Encode.string(str(user_id))
+        print(encoded_user_id)
+        
+if __name__ == '__main__':
+    UserToken.generate(80088516616269824)

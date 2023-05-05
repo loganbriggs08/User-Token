ce2dacf64db2d76c15addf422197689b96e23818
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-05 20:26:54 +0100

    Encode passed through user_id

--- usertoken.py
+++ usertoken.py
@@ -2,8 +2,10 @@ from helpers.encoding import Encode
 
 class UserToken:
     def generate(user_id: int) -> str:
-        encoded_user_id = Encode.string(str(user_id))
-        print(encoded_user_id)
+        token_structure: str = f""
+        
+        token_structure += f"{Encode.string(str(user_id))}."
+        print(token_structure)
         
 if __name__ == '__main__':
     UserToken.generate(80088516616269824)
